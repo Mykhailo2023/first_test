9843fc40cbd03c5e7f76be704faf6659dac83986
Mykhailo2023 <152901832+Mykhailo2023@users.noreply.github.com>
2023-12-26 10:28:40 +0200

    Create init.py

---
+++ init.py
@@ -0,0 +1,10 @@
+n = int(input("Введіть кількість секунд:"))
+
+
+hours = n // (60 * 60)
+minutes = (n - hours * 60 * 60) // 60
+seconds = n - hours * 60 * 60 - minutes * 60
+print("З", n, "секунд буде:")
+print(hours, "годин")
+print(minutes, "хвилин")
+print(seconds, "Секунд")
