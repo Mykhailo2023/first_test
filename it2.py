43b72df92d0407b860fc5c4e91db7c18937825d3
Mykhailo2023 <152901832+Mykhailo2023@users.noreply.github.com>
2024-01-08 10:32:42 +0200

    Add a new text file

---
+++ it2.py
@@ -0,0 +1,34 @@
+cat = {}
+info = {"status": "vaccinated", "breed": True}
+cat ["nick"] = "Simon"
+cat ["age"] = 7
+cat ["characteristics"] = ["лагідний", "кусається"]
+age = cat.get('age')
+cat.update(info)
+print(cat)
+print(age)
+
+
+
+#data = {'year': 2024, 'lang': 'Python', 'version': 3.12}
+
+#print(data)
+
+
+#my_list = [2024, 3.12]
+#some_data = ['Python']
+#my_list.extend(some_data)
+#my_list.insert(1, 'Python')
+#my_list.reverse
+
+#print(my_list)
+
+
+
+#my_list = []
+#my_list.insert(0, 2024)
+#my_list.insert(1, 'Python')
+#my_list.insert(2, 3.12)
+
+
+#print(my_list)
